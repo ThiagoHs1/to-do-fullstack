140805944fd7a8d726c5fd505ce35e70a353878a
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-10-05 09:49:40 -0300

    estilizando todolist e addtodo

---
+++ front-end/src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container mx-auto mt-20 w-3/5">
+      <div class="add-todo bg-white shadow-md rounded-lg p-4  items-center ">
+        <input  
+          type="text" 
+          v-model="newTodoText" 
+          placeholder="Nova Tarefa" 
+          class="todo-input px-4 py-2 border border-gray-300 rounded-md w-3/5"
+          @keyup.enter="addTodo"
+        />
+        <button @click="addTodo" class="add-button bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition w-1/5 ml-4">Adicionar</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    data() {
+      return {
+        newTodoText: ''
+      }
+    },
+    methods: {
+      addTodo() {
+        // Lógica para adicionar tarefa
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos adicionais se necessário */
+  </style>
+  
--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
+import AddTodo from '@/components/AddTodo.vue';
+import TodoList from '@/components/TodoList.vue';
 </script>
 
 <template>
   <NavBar />
+  <AddTodo />
+  <TodoList />
   
 </template>
